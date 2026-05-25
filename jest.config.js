4f35dc2a4f9afd617d42b19b4ec45703d057289b
donato81 <forbicedonato@gmail.com>
2026-05-25 07:45:05 +0200

    chore(jest): aggiorna la configurazione per includere il mock di async-storage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'react-native',
-  setupFiles: ['react-native-get-random-values'],
+  setupFiles: [
+    'react-native-get-random-values',
+  ],
+  moduleNameMapper: {
+    '@react-native-async-storage/async-storage': require.resolve(
+      '@react-native-async-storage/async-storage/jest/async-storage-mock'
+    ),
+  },
 };
